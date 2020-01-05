7001f24014961f2a82d31754775301c43f648af1
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-01-05 00:45:27 +0000

    MVC Event App Project
    Starting MVC EventApp Project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheEventApp.Startup))]
+namespace TheEventApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
