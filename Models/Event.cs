13513376bfa227277ee9404543de3bb45c85cf13
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-01-10 14:53:32 +0000

    Added Glimpse, worked on variables for Event Class and added Controller & Views windows

---
+++ Models/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace TheEventApp.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+
+    public class EventContext : DbContext
+    {
+        public EventContext()
+        {
+            Database.Log = s => Debug.WriteLine(s);
+        }
+        public DbSet<Event> Events { get; set; }
+        
+    }
+}
