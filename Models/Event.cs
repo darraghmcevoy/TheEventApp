141fe205a6a970b21ff05a612bea0802af55949c
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-01-15 20:44:39 +0000

    New Data Annotations to Event Class :)

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
@@ -9,11 +10,27 @@ namespace TheEventApp.Models
 {
     public class Event
     {
+        [Required(ErrorMessage = "Please Enter Your EventID"), MaxLength(30)]
+        [Display(Name = "Event ID")]
         public int EventID { get; set; }
-
+        [Required(ErrorMessage = "Please enter the name of this event")]
+        [Display(Name = "Event Name")]
+        [MaxLength(30)]
         public string Title { get; set; }
-
+        
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
         public DateTime Date { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Please enter your Email address")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
+
+
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your mobile number")]
+        [Display(Name = "Contact number")]
+        [DataType(DataType.PhoneNumber)]
+        public string Mobile { get; set; }
+       
     }
 
     public class EventContext : DbContext
